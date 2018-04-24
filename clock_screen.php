aec9f94beb7e50260039cd4a0502027c033279ce
rosh <roshmedia@gmail.com>
2018-04-24 20:54:40 +0100

    This is my stage 1 results. A PHP coded clock in html. 3 files

---
+++ clock_screen.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My phone screen</title>
+
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+<style>
+
+</style>
+
+</head>
+<body>
+
+<div id="GS8">
+
+	<p id="lagos_time">
+		<?php
+			$lagos_t=time();
+			echo(date("h:i",$lagos_t));
+		?> 
+</p>
+
+ </div>
+
+ <img src="./my_phone_1" alt = "my phone screen">
+
+
+ 
+ 
+<!--
+-->
+
+</body>
+</html>
